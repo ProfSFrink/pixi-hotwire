1f414c8f0dc97fdcc4a9cf3ca0ea1f6cf1b874fb
eCode <miltoncandelero@gmail.com>
2024-04-06 13:56:25 -0300

    fix: top level await doesn't quite work, not sure why

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Application, Assets, Renderer, Sprite } from 'pixi.js'
 
-const app = new Application<Renderer<HTMLCanvasElement>>();
+// Top-level async function for compatibility
+async function main() {
 
-// Top Level Await
+const app = new Application<Renderer<HTMLCanvasElement>>();
 
 await app.init({
 	canvas: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -13,7 +14,6 @@ await app.init({
 	height: 480,
 	hello:true // Added so you can see what renderer are you using
 });
-
 
 // V8 change: You MUST load first. You can't use the `from` method without loading first.
 await Assets.load("clampy.png");
@@ -26,3 +26,8 @@ clampy.x = app.screen.width / 2;
 clampy.y = app.screen.height / 2;
 
 app.stage.addChild(clampy);
+
+}
+
+// Call the main function
+main();
