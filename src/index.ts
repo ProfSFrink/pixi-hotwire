167deb0b599ffeda5cf44119dfb674033db6d68f
Steven Partlow <ProfSFrink@Gmail.com>
2024-05-02 15:10:22 +0000

    The DisplayList

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Sprite, Container } from "pixi.js";
 
 const app = new Application<HTMLCanvasElement>({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: 640,
+  height: 480,
 });
 
+const conty: Container = new Container();
+conty.x = 200;
+conty.y = 0;
+app.stage.addChild(conty);
+
 const clampy: Sprite = Sprite.from("clampy.png");
-
-clampy.anchor.set(0.5);
-
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-
-app.stage.addChild(clampy);
+clampy.x = 100;
+clampy.y = 100;
+conty.addChild(clampy);
